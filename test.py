e104f08a74d86abfc508b832e31b9d6c140d195c
noodlefighter <noodlefighter@gmail.com>
2021-08-07 16:14:54 +0800

    修复执行 add 命令时，若指定项目已在 ignore 列表中，不会从 ignore 中剔除项目的问题

--- test.py
+++ test.py
@@ -11,12 +11,14 @@ def str_to_list(text):
     l = str(text).split()
     return list(filter(lambda x: x != '', map(lambda x: x.strip(), l)))
 
+def new_environment():
+    exec_cmd('rm test.json')
 
 test_cmd = "python ./pkgsync test.json "
 
 class TestStringMethods(unittest.TestCase):
     def test_add_and_del(self):
-        exec_cmd('rm test.json')
+        new_environment()
 
         exec_cmd(test_cmd + 'add 1 2 3')
         ls = str_to_list(exec_cmd(test_cmd + 'list-sync'))
@@ -32,7 +34,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue('3' not in ls)
 
     def test_ignore_and_del(self):
-        exec_cmd('rm test.json')
+        new_environment()
 
         exec_cmd(test_cmd + 'ignore 1 2 3')
         ls = str_to_list(exec_cmd(test_cmd + 'list-ignore'))
@@ -47,8 +49,14 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue('2' not in ls)
         self.assertTrue('3' not in ls)
 
+        exec_cmd(test_cmd + 'add 1')
+        ls = str_to_list(exec_cmd(test_cmd + 'list-ignore'))
+        self.assertTrue('1' not in ls)
+        ls = str_to_list(exec_cmd(test_cmd + 'list-sync'))
+        self.assertTrue('1' in ls)
+
     def test_pkglist_cmd_and_list_pending(self):
-        exec_cmd('rm test.json')
+        new_environment()
 
         exec_cmd(test_cmd + "set-pkglist-cmd 'echo 123 456'")
         ls = str_to_list(exec_cmd(test_cmd + 'list-pending'))
